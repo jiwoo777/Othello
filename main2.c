de0c0b52a4d5ab9ec5425d02c88fdb80604320d6
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-26 00:00:36 +0900

    보드판 구현 출력까지

--- main2.c
+++ main2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(void) { 
 
-   int array[8][8]=
-   {
+void main(void) { 
+
+   int board[8][8] = {//보드판 만들기 
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0},
@@ -15,34 +16,26 @@ int main(void) {
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0}}; 
      
-    int Board(int array[8][8])//판 그리기 함수 
-   {
       int i, j;
    
       for(i=0; i<8; i++)
       {
-   	    gotoxy(2,2+2*i);//좌표로 커서 이동 
-   	 
+   	       	 
    	    for (j=0; j<8; j++)
-   	    {
-   	    	if(array[i][j] == 0)//돌 없는 빈공간 
-   	 	        printf("| |");
-   	 	   else if(array[i][j] == 1)// 흰돌 둔 곳 
+   	    {   	    	
+   	    	if(board[i][j] == 0)//돌 없는 빈공간 표시 
+   	 	        printf("|_|");
+   	 	   else if(board[i][j] == 1)// 흰돌 둔 곳 표시 
 		        printf("|O|");
-		   else if(array[i][j] == 2)// 검은돌 둔 곳 
-		    	printf("|X|");
-       	   else if (array[i][j] == 3)
-		        printf("|_|" );//돌을 놓을 수 있는 자리 표시			
+		   else if(board[i][j] == 2)// 검은돌 둔 곳 표시 
+		    	printf("|X|");   		
 	    } 
-	     printf("\n\n");
-    	 gotoxy(35,9);
-    	 printf("일시정지[K]");
-	   
-    } 
-    return 0;
+	     printf("\n");
+      }
+      return board[8][8]; 
+    }
+   
 
-   }
-}
 
 
 
