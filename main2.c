c653a5b9ec98641b530701a06aacfcd1f6235b7d
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-26 05:04:11 +0900

    커서 이동 구현..?

--- main2.c
+++ main2.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <conio.h>
+#define N 6
+#define true 1
+ 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void) { 
 
-   int board[8][8] = {//보드판 만들기 
+	int board[8][8] = {//초기 보드판 만들기 
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0},
@@ -16,27 +20,62 @@ void main(void) {
      {0,0,0,0,0,0,0,0},
      {0,0,0,0,0,0,0,0}}; 
      
-      int i, j;
+      int row, column;//가로줄 세로줄 변수 설정  
    
-      for(i=0; i<8; i++)
+      for(row=0; row<8; row++)
       {
    	       	 
-   	    for (j=0; j<8; j++)
+   	    for (column=0; column<8; column++)
    	    {   	    	
-   	    	if(board[i][j] == 0)//돌 없는 빈공간 표시 
+   	    	if(board[row][column] == 0)//돌 없는 빈공간 표시 
    	 	        printf("|_|");
-   	 	   else if(board[i][j] == 1)// 흰돌 둔 곳 표시 
+   	 	   else if(board[row][column] == 1)// 흰돌 둔 곳 표시 
 		        printf("|O|");
-		   else if(board[i][j] == 2)// 검은돌 둔 곳 표시 
+		   else if(board[row][column] == 2)// 검은돌 둔 곳 표시 
 		    	printf("|X|");   		
 	    } 
 	     printf("\n");
       }
       return board[8][8]; 
+      
+      
+      char Choice = 0;//선택한 지점  정의  
+      int x = 0, y = 0;//x좌표, y좌표   
+      while(true)
+      {
+      	gotoxy(x,y);//변수 x와 y에 저장된 값으로 좌표를 이동
+		printf("+");//커서 표시
+		if(kbhit())
+		{
+		    Choice = getch();
+			switch(Choice)
+			{
+				case 'w' : //위로 이동
+				   y--;
+				   break;
+				case 's' : //아래로 이동
+				   y++;
+				   break;
+				case 'a' : //왼쪽으로 이동
+				   x--;
+				   break;
+				case 'd' : //오른쪽으로 이동 
+				   x++;
+				   break;         
+			}
+			Sleep(50); 
+			system("cls");
+		   }   
+	  }
+	  return 0;
     }
-   
-
-
+    
+    
+void gotoxy(int x, int y)//커서 이동 함수  
+{
+	COORD Cursor={x,y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cursor);
+ } 
 
 
 
